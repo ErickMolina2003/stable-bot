9a06e1f51710ce003d62b2d84646a63db7d2609e
Erick Molina <erick.molina@01da.de>
2023-02-13 21:08:24 -0600

    Initial files

---
+++ src/views/chat-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Chat</h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "ChatPage",
+})
+export default class ChatPage extends Vue {}
+</script>
+
+<style scoped></style>
---
+++ src/views/landing-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "LandingPage",
+})
+export default class LandingPage extends Vue {}
+</script>
+
+<style scoped></style>
